2db70e784ed068d05af2d8b7b6dc850b474d9fbf
Sumeet Bhagat <143217758+SumeetB1811@users.noreply.github.com>
2024-03-27 01:06:09 +0530

    Update client.py
    Added lines to connect different clients on the server thru server code.

--- client.py
+++ client.py
@@ -1,20 +1,30 @@
 import socket
 
-socketObject = socket.socket()
+header = 64
+port = 5050
+format = 'utf - 8'
+disconnect_message = "Connection lost"
 
-socketObject.connect(("localhost", 5050))
-print("Connected to localhost")
+server_code = input('Enter the code: ')                           #for geeting to connected to the server.
+server_code = str(server_code)
+server = server_code
 
-msg = "Hi Server"
-Bytes = str.encode(msg)
+Addr = (server,port)
 
-socketObject.sendall(Bytes)
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(Addr)
+print('Connection Stablished... ')
 
-while(True):
-    data = socketObject.recv(1024)
-    print(data)
+def send(msg):
+    message = msg.encode(format)
+    msg_len = len(message)
+    send_len = str(msg_len).encode(format)
+    send_len += b' ' *  (header - len(send_len))
+    client.send(send_len)
+    client.send(message)
+    print(client.recv(2048).decode(format))
 
-    if not data:
-        print("Connection closed")
-        break
-socketObject.close()
+send(input('Enter your Game Name: '))
+input()
+
+send(disconnect_message)
