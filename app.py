11b0d77fc20586f175c3453494b22620e7a22376
Shreyash Gardi <147970208+Cyntax-0@users.noreply.github.com>
2024-04-19 02:19:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+mysql_host = 'localhost'
+mysql_user = 'root'
+mysql_password = ''
+mysql_db = 'quizzaroo'
+
+db_connection = mysql.connector.connect(
+    host=mysql_host,
+    user=mysql_user,
+    password=mysql_password,
+    database=mysql_db
+)
+
+cursor = db_connection.cursor()
+
+@app.route('/')
+def opening():
+    return render_template('start.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+        user = cursor.fetchone()
+
+        if user:
+            if user[3] == password:
+                # Password is correct, redirect to home
+                return redirect(url_for('home'))
+            else:
+                # Password is incorrect
+                error_message = "Invalid email or password. Please try again."
+                return render_template('login.html', error_message=error_message)
+        else:
+            # User doesn't exist
+            error_message = "User does not exist. Please sign up."
+            return render_template('login.html', error_message=error_message)
+
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        insert_query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
+        cursor.execute(insert_query, (name, email, password))
+        db_connection.commit()
+
+        return redirect(url_for('login'))
+    return render_template('regis.html')
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/join')
+def join_quiz():
+    return render_template('join.html')
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('opening'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Login function
+    function login() {
+        window.location.href = "/login";
+    }
+    const loginButton = document.querySelector("button[onclick='login();']");
+    loginButton.addEventListener("click", login);
+
+    // Signin function
+    function signin() {
+        window.location.href = "/signup";
+    }
+    const signButton = document.querySelector("button[onclick='signin();']");
+    signButton.addEventListener("click", signin);
+
+    // Join function
+    function join() {
+        window.location.href = "/join";
+    }
+    const joinButton = document.querySelector("button[onclick='join();']");
+    joinButton.addEventListener("click", join);
+    
+    function home() {
+        window.location.href = "/home";
+    }
+    const homeButton = document.querySelector("button[onclick='home();']");
+    homeButton.addEventListener("click", home);
+});
